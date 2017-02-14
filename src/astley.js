14f3a4d314c780b2e154b86297f665c72f1859a1
Brandon Wong <b.lh.wong@gmail.com>
2017-02-13 20:33:08 -0800

    Add astley subclass

---
+++ src/astley.js
@@ -0,0 +1,12 @@
+var makeAstley = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+makeAstley.prototype = Object.create(makeDancer.prototype);
+makeAstley.prototype.constructor = makeAstley;
+makeAstley.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  this.$node.toggle();
+};
